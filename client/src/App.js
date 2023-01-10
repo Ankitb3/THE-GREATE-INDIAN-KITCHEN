fd193bef51c4cff04037ff576939ba034bc6ca14
ankit <bankarankit3@gmail.com>
2023-01-10 17:31:15 +0530

    rotes added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './views/home/Home';
+import Singup from './views/singup/Singup';
+import Login from './views/login/Login';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/singup" element={<Singup/>} />
+          <Route path="/login" element={<Login/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App
